80c6217fe566e6d819d9ca4beff0b5ae46e6ebea
Tristan Lucero <tristanandrewlucero@gmail.com>
2021-03-18 19:34:08 -0500

    adding browser features such as back, forward, refresh, and adding some functionality to search bar but search bar is not fully complete

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
