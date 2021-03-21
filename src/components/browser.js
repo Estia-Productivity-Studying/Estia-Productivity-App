240d14ea1c60288ddd8f58922e899f716c7d4fca
Tristan Lucero <tristanandrewlucero@gmail.com>
2021-03-21 18:36:22 -0500

    added blacklist, and added checks needed for blacklist to check if URL is in blacklist and not load it if it is in the blacklist.

--- src/components/browser.js
+++ src/components/browser.js
@@ -10,19 +10,25 @@ import RefreshIcon from '@material-ui/icons/Refresh';
 import Input from '@material-ui/core/Input';
 import validator from 'validator'
 
+// Function to handle clicks on breadcrumbs
 function handleClick(event) {
     console.info('You clicked a breadcrumb.');
 }
 
 class BrowserWindow extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        //Get current settings from database
-        blacklist: [],
-      };
+        super(props);
+        this.state = {
+            //Get current settings from database
+            //placeholder data, should get replaced by backend
+            blacklist: ["twitter.com"]
+        };
     }
-      // Function to handle clicks on breadcrumbs
+
+    //getter to get blacklist
+    getBlacklist() {
+        return this.state.blacklist
+    }
     
     // Functions to handle clicks for web pages, back, forward, and reload
     page_back(event){
@@ -42,13 +48,15 @@ class BrowserWindow extends React.Component {
     
     // function to handle you press enter on the search bar
     load_url(event){
+        //checks if key pressed is enter, if so checks for valid url that's not in blacklist
+        //if everything looks goo then loads the url with webview
         if (event.key === 'Enter'){
-            if (validator.isURL("http://" + event.target.value)){
+            if (validator.isURL("http://" + event.target.value) && !this.getBlacklist().includes(event.target.value)){
                 const webview = document.querySelector('webview')
                 webview.loadURL("http://" + event.target.value)
                 event.target.placeholder = ""
             }
-            else if (validator.isURL(event.target.value)){
+            else if (validator.isURL(event.target.value) && !this.getBlacklist().includes(event.target.value)){
                 const webview = document.querySelector('webview')
                 webview.loadURL(event.target.value)
                 event.target.placeholder = ""
@@ -62,7 +70,7 @@ class BrowserWindow extends React.Component {
 
     render() {
         return (
-        <div style={{ display: 'flex', flex: 9.5, 'flex-flow': 'column' }}>
+        <div style={{ display: 'flex', flex: 9.5, flexFlow: 'column' }}>
             {/* Div for browser control buttons */}
             <div style={{ flex: '0.1' }}>
             <IconButton variant="outlined" size="small" onClick={ this.page_back }>
@@ -76,7 +84,7 @@ class BrowserWindow extends React.Component {
             </IconButton>
             {/* This is for the browser search bar */}
             <Input 
-                onKeyDown={ this.load_url } 
+                onKeyDown={this.load_url.bind(this)} 
                 id="url" 
                 label="url" 
                 variant="outlined" />
@@ -91,7 +99,7 @@ class BrowserWindow extends React.Component {
 
 function BrowserPage() {
     return (
-            <div class='browserDiv' style={{ display: 'flex', 'flex-direction': 'column'}}>
+            <div className='browserDiv' style={{ display: 'flex', flexDirection: 'column'}}>
                 {/* Breadcrumbs div for the header */}
                 <div style={{ flex: 0.5 }}>
                     <Typography variant="h6" component="h6">
