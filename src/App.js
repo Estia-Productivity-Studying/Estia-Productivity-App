0fe2dc46ec95779c85b300df8c2c039a2f84cbea
Tristan Lucero <tristanandrewlucero@gmail.com>
2021-03-23 16:35:31 -0500

    actually implemented tabs that don't re-render, still need to fix CSS with browser page but everything else should work.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import { createStyles } from "@material-ui/core/styles";
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { useMemo } from 'react';
 
 import LandingPage from './components/landingpage';
 import BrowserPage from './components/browser';
@@ -15,85 +15,80 @@ import NotepadPage from './components/notepad';
 import SettingsPage from './components/settings';
 import TimerPage from './components/timer';
 
+const useStyles = makeStyles({
+  root: {
+  },
+});
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+const useTabContainerStyles = makeStyles((theme) =>  createStyles({
+  root: {
+    padding: 8 * 3
+  },
+  tabcontainerInActive: {
+    display: "none"
+  }
+})
+);
 
+function TabContainer(props) {
+  const styles = useTabContainerStyles({});
+  console.log("In TabContainer");
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
+    <Typography
+      id={props.id.toString()}
+      component="div"
+      className={(styles.root, {
+        [styles.tabcontainerInActive]: !props.active
+      })}
     >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
+      {props.children}
+    </Typography>
   );
 }
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
 
 export default function SimpleTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const classes = useStyles({});
+  const [selectedTab, setSelectedTab] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  function handleChange(event, newValue) {
+    setSelectedTab(newValue);
+}
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static" color="inherit">
-        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Landing Page" {...a11yProps(0)} />
-          <Tab label="Browser" {...a11yProps(1)} />
-          <Tab label="Calendar" {...a11yProps(2)} />
-          <Tab label="Timer" {...a11yProps(3)} />
-          <Tab label="Music" {...a11yProps(4)} />
-          <Tab label="Settings" {...a11yProps(5)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <LandingPage/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <BrowserPage/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <CalendarPage/>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        <TimerPage/>
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        <MusicPage/>
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        <SettingsPage/>
-      </TabPanel>
-    </div>
-  );
+return (
+  <div className={classes.root}>
+    <AppBar position="static" color="inherit">
+      <Tabs value={selectedTab} onChange={handleChange}>
+        <Tab label="Landing" />
+        <Tab label="Browser" />
+        <Tab label="Calendar" />
+        <Tab label="Music" />
+        <Tab label="Notepad" />
+        <Tab label="Timer" />
+        <Tab label="Settings" />
+      </Tabs>
+    </AppBar>
+
+    <TabContainer id={0} active={selectedTab === 0}>
+      <LandingPage/>
+    </TabContainer>
+    <TabContainer id={1} active={selectedTab === 1}>
+      <BrowserPage/>
+    </TabContainer>
+    <TabContainer id={2} active={selectedTab === 2}>
+      <CalendarPage/>
+    </TabContainer>
+    <TabContainer id={3} active={selectedTab === 3}>
+      <MusicPage/>
+    </TabContainer>
+    <TabContainer id={3} active={selectedTab === 3}>
+      <NotepadPage/>
+    </TabContainer>
+    <TabContainer id={4} active={selectedTab === 4}>
+      <TimerPage/>
+    </TabContainer>
+    <TabContainer id={5} active={selectedTab === 5}>
+      <SettingsPage/>
+    </TabContainer>
+  </div>
+);
 }
