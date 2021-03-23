0ec284194eef6af4d393808b10f62dba166f3428
Hy-5 <coulbaly.ismael@gmail.com>
2021-03-23 15:41:52 -0500

    used the login page template with minor modifications

--- src/components/loginpage.js
+++ src/components/loginpage.js
@@ -1,63 +1,163 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from "@material-ui/core/Checkbox";
-import Grid from '@material-ui/core/Grid'
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 function handleClick(event) {
-  console.info("You clicked a breadcrumb.");
+  console.info('You clicked a breadcrumb.');
 }
 
-const StyledSignButton = withStyles({
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 56,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  },
-  label: {
-    textTransform: 'capitalize',
-  },
-  positions: {
-    bottom: 10,
-    right: 50,
-  },
-})(Button);
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Estia
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 
-const UserNameBox =(TextField);
-
-function LoginPage() {
-  return (
-    <div>
-      <Grid item xs={4} style={{textAlign:"center"}}>
-      <form className={UserNameBox} noValidate autoComplete="off">
-        <TextField id="Username" label="Username" variant="filled"/>
-        <TextField id="Password" label="Password" variant="filled"/>
-      </form>
-      </Grid>
-
-      <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-      />
-      <Grid item xs={4} style={{textAlign:"center"}}>
-      <StyledSignButton variant="contained" color="primary">
-        Sign In
-      </StyledSignButton>
-      </Grid>
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 
-      <StyledSignButton variant="contained" color="primary" href="/landingpage" onClick={handleClick}>
-        (Login WIP) -Skip-
-      </StyledSignButton>
+function LandingPage() {
+    const classes = useStyles();
+    return (
+        <div>
+        <Typography variant="h6" component="h6">
+            Main Menu_Landing Page:
+        </Typography>
+        <Breadcrumbs aria-label="breadcrumb">
+            <Link color="inherit" href="/landingpage" onClick={handleClick}>
+                Main Menu
+            </Link>
+            <Link color="inherit" href="/timer" onClick={handleClick}>
+                Timer
+            </Link>
+            <Link color="inherit" href="/browser" onClick={handleClick}>
+                Browser
+            </Link>
+            <Link color="inherit" href="/notepad" onClick={handleClick}>
+                Notepad
+            </Link>
+            <Link color="inherit" href="/calendar" onClick={handleClick}>
+                Calendar
+            </Link>
+            <Link color="inherit" href="/music" onClick={handleClick}>
+                Music
+            </Link>
+            <Link color="inherit" href="/settings" onClick={handleClick}>
+                Settings
+            </Link>
+        </Breadcrumbs>
+        <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+
+            <Link
+              href="/landingpage"
+              variant="body2"
+              color="secondary"
+              onClick={handleClick}
+            >
+              CLICK HERE TO SKIP
+            </Link>
+
+          </Grid>
+        </form>
       </div>
-  );
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+        </div>
+    );
 }
 
 
-export default LoginPage;
+export default LandingPage;
