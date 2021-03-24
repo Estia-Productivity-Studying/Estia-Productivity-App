32dae4e472a146d65f5b064efc32141b835001d2
bradenmoss <60902194+bradenmoss@users.noreply.github.com>
2021-03-24 14:39:49 -0500

    Update App.js
    Styling tabs.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { createStyles } from "@material-ui/core/styles";
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import { useMemo } from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import { useMemo } from "react";
 
-import LoginPage from './components/loginpage';
-import LandingPage from './components/landingpage';
-import BrowserPage from './components/browser';
-import CalendarPage from './components/calendar';
-import MusicPage from './components/music';
-import NotepadPage from './components/notepad';
-import SettingsPage from './components/settings';
-import TimerPage from './components/timer';
+import LoginPage from "./components/loginpage";
+import LandingPage from "./components/landingpage";
+import BrowserPage from "./components/browser";
+import CalendarPage from "./components/calendar";
+import MusicPage from "./components/music";
+import NotepadPage from "./components/notepad";
+import SettingsPage from "./components/settings";
+import TimerPage from "./components/timer";
 
-const useStyles = makeStyles({
-  root: {
+const useStyles = makeStyles((theme) => ({
+  indicator: {
+    backgroundColor: "#121212",
+    height: "5px",
   },
-});
-
-const useTabContainerStyles = makeStyles((theme) =>  createStyles({
-  root: {
-    padding: 8 * 3
+  tabsWrapper: {
+    background: "#3eb489",
   },
-  tabcontainerInActive: {
-    display: "none"
-  }
-})
+}));
+
+const useTabContainerStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      padding: 8 * 3,
+    },
+    tabcontainerInActive: {
+      display: "none",
+    },
+  })
 );
 
 function TabContainer(props) {
@@ -38,9 +44,12 @@ function TabContainer(props) {
     <Typography
       id={props.id.toString()}
       component="div"
-      className={(styles.root, {
-        [styles.tabcontainerInActive]: !props.active
-      })}
+      className={
+        (styles.root,
+        {
+          [styles.tabcontainerInActive]: !props.active,
+        })
+      }
     >
       {props.children}
     </Typography>
@@ -48,52 +57,59 @@ function TabContainer(props) {
 }
 
 export default function SimpleTabs() {
-  const classes = useStyles({});
+  const classes = useStyles();
   const [selectedTab, setSelectedTab] = React.useState(0);
 
-  function handleChange(event, newValue) {
-    setSelectedTab(newValue);
-}
+  function handleChange(event, newSelectedTab) {
+    setSelectedTab(newSelectedTab);
+  }
 
-return (
-  <div className={classes.root}>
-    <AppBar position="static" color="inherit">
-      <Tabs value={selectedTab} onChange={handleChange}>
-        <Tab label="Logout" />
-        <Tab label="Landing" />
-        <Tab label="Browser" />
-        <Tab label="Calendar" />
-        <Tab label="Music" />
-        <Tab label="Notepad" />
-        <Tab label="Timer" />
-        <Tab label="Settings" />
-      </Tabs>
-    </AppBar>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="inherit">
+        <Tabs
+          value={selectedTab}
+          onChange={handleChange}
+          className={classes.tabsWrapper}
+          classes={{
+            indicator: classes.indicator,
+          }}
+        >
+          <Tab label="Logout" />
+          <Tab label="Landing" />
+          <Tab label="Browser" />
+          <Tab label="Calendar" />
+          <Tab label="Music" />
+          <Tab label="Notepad" />
+          <Tab label="Timer" />
+          <Tab label="Settings" />
+        </Tabs>
+      </AppBar>
 
-    <TabContainer id={0} active={selectedTab === 0}>
-      <LoginPage/>
-    </TabContainer>
-    <TabContainer id={0} active={selectedTab === 1}>
-      <LandingPage/>
-    </TabContainer>
-    <TabContainer id={1} active={selectedTab === 2}>
-      <BrowserPage/>
-    </TabContainer>
-    <TabContainer id={2} active={selectedTab === 3}>
-      <CalendarPage/>
-    </TabContainer>
-    <TabContainer id={3} active={selectedTab === 4}>
-      <MusicPage/>
-    </TabContainer>
-    <TabContainer id={3} active={selectedTab === 5}>
-      <NotepadPage/>
-    </TabContainer>
-    <TabContainer id={4} active={selectedTab === 6}>
-      <TimerPage/>
-    </TabContainer>
-    <TabContainer id={5} active={selectedTab === 7}>
-      <SettingsPage/>
-    </TabContainer>
-  </div>
-);
+      <TabContainer id={0} active={selectedTab === 0}>
+        <LoginPage />
+      </TabContainer>
+      <TabContainer id={0} active={selectedTab === 1}>
+        <LandingPage />
+      </TabContainer>
+      <TabContainer id={1} active={selectedTab === 2}>
+        <BrowserPage />
+      </TabContainer>
+      <TabContainer id={2} active={selectedTab === 3}>
+        <CalendarPage />
+      </TabContainer>
+      <TabContainer id={3} active={selectedTab === 4}>
+        <MusicPage />
+      </TabContainer>
+      <TabContainer id={3} active={selectedTab === 5}>
+        <NotepadPage />
+      </TabContainer>
+      <TabContainer id={4} active={selectedTab === 6}>
+        <TimerPage />
+      </TabContainer>
+      <TabContainer id={5} active={selectedTab === 7}>
+        <SettingsPage />
+      </TabContainer>
+    </div>
+  );
 }
