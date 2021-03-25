318ab7b30da20ef2b3c9b1b362315336e87f540f
bradenmoss <60902194+bradenmoss@users.noreply.github.com>
2021-03-25 12:06:25 -0500

    Dynamic Tabs in Browser Tab

--- src/components/browser.js
+++ src/components/browser.js
@@ -1,17 +1,91 @@
-/* Browser - Tristan Lucero */
 import React, { Fragment } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import RefreshIcon from "@material-ui/icons/Refresh";
+import AddIcon from "@material-ui/icons/Add";
 import Input from "@material-ui/core/Input";
 import validator from "validator";
-import { withWidth } from "@material-ui/core";
+import { makeStyles, AppBar, Tabs, Tab, Paper, Box } from "@material-ui/core";
+import { PostAdd } from "@material-ui/icons";
 import "./css/browser.css";
 
 // Function to handle clicks on breadcrumbs
 function handleClick(event) {
   console.info("You clicked a breadcrumb.");
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+}));
+
+let maxTabIndex = 0;
+let currentTablIndex = 0;
+
+function TabPanel(props) {
+  const { children, tabId } = props;
+  return (
+    <Box
+      value={maxTabIndex}
+      index={maxTabIndex}
+      hidden={tabId !== currentTablIndex}
+      key={maxTabIndex}
+    >
+      {children}
+    </Box>
+  );
+}
+
+function Pipeline(props) {
+  const classes = useStyles();
+
+  // Handle Tab Button Click
+  const [tabId, setTabId] = React.useState(0);
+  const handleTabChange = (event, newTabId) => {
+    if (newTabId === "tabProperties") {
+      handleAddTab();
+    } else {
+      currentTablIndex = newTabId;
+      setTabId(newTabId);
+    }
+  };
+
+  // Handle Add Tab Button
+  const [tabs, setAddTab] = React.useState([]);
+  const handleAddTab = () => {
+    maxTabIndex = maxTabIndex + 1;
+    setAddTab([
+      ...tabs,
+      <Tab label={`New Tab ${maxTabIndex}`} key={maxTabIndex} />,
+    ]);
+    handleTabsContent();
+  };
+
+  // Handle Add Tab Content
+  const [tabsContent, setTabsContent] = React.useState([<BrowserWindow />]);
+  const handleTabsContent = () => {
+    setTabsContent([<BrowserWindow />]);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <AppBar position="static" color="inherit">
+        <Tabs
+          value={tabId}
+          onChange={handleTabChange}
+          variant="scrollable"
+          scrollButtons="on"
+        >
+          <Tab label="Default" />
+          {tabs.map((child) => child)}
+          <Tab icon={<PostAdd />} value="tabProperties" />
+        </Tabs>
+      </AppBar>
+      <Box padding={2}>{tabsContent.map((child) => child)}</Box>
+    </Paper>
+  );
 }
 
 class BrowserWindow extends React.Component {
@@ -75,7 +149,7 @@ class BrowserWindow extends React.Component {
     return (
       <div style={{ display: "flex", flex: 9.5, flexFlow: "column" }}>
         {/* Div for browser control buttons */}
-        <div style={{ flex: "0.1" }}>
+        <div id="web-bar" style={{ flex: "0.1" }}>
           <IconButton variant="outlined" size="small" onClick={this.page_back}>
             <ArrowBackIosIcon style={{ color: "#3eb489" }}></ArrowBackIosIcon>
           </IconButton>
@@ -103,6 +177,13 @@ class BrowserWindow extends React.Component {
             variant="outlined"
             type="search"
           />
+          <IconButton
+            variant="outlined"
+            size="small"
+            onClick={this.newTabHandler}
+          >
+            <AddIcon style={{ color: "#3eb489" }}></AddIcon>
+          </IconButton>
         </div>
         {/* Build in browser, uses webview from Electron */}
         <webview
@@ -116,7 +197,11 @@ class BrowserWindow extends React.Component {
 }
 
 function BrowserPage() {
-  return <BrowserWindow />;
+  return (
+    <div>
+      <Pipeline />
+    </div>
+  );
 }
 
 export default BrowserPage;
