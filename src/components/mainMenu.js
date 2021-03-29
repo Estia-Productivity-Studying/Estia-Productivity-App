c96958d56da12d23eba133fe298383c70bf5dda3
Hy-5 <coulbaly.ismael@gmail.com>
2021-03-29 02:27:03 -0500

    Created main menu and split it in half

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -2,57 +2,24 @@ import React from "react";
 import "./css/mainMenu.css";
 
 class Menu extends React.Component {
-  saveTextAsFile() {
-    var textToSave = document.getElementById("inputTextToSave").value;
-    var textToSaveAsBlob = new Blob([textToSave], { type: "text/plain" });
-    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
-    var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs")
-      .value;
-    var downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "Download File";
-    downloadLink.href = textToSaveAsURL;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-  }
-
-  loadFileAsText() {
-    var fileToLoad = document.getElementById("fileToLoad").files[0];
-    var fileReader = new FileReader();
-    if (document.getElementById("fileToLoad").files.length !== 0) {
-      fileReader.onload = function (fileLoadedEvent) {
-        var textFromFileLoaded = fileLoadedEvent.target.result;
-        document.getElementById("inputTextToSave").value = textFromFileLoaded;
-      };
-      fileReader.readAsText(fileToLoad, "UTF-8");
-    } else {
-      alert("Please Select a File.");
-    }
-  }
-
   render() {
-    return (
+    return(
       <div>
-        <h1 id="menu-header">Menu</h1>
-        <div id="wrap">
-          <textarea id="inputTextToSave" cols="1000" rows="30"></textarea>
+        <div class="split left">
+          <div class="centered">
+          <img src="../temporary_image/folders.jpg" alt="Folders"/>
+          <h2>Folders side</h2>
+          <p>Some text.</p>
+          </div>
         </div>
-        <h3 id="input-header">Filename to Save As: </h3>
-        <input id="inputFileNameToSaveAs" />
-        <br />
-        <button id="button" onClick={this.saveTextAsFile}>
-          Save Text to File
-        </button>
-        <br />
-        <br />
-        <h3 id="input-header">Select a File to Load: </h3>
-        <input id="file-load" type="file" id="fileToLoad" />
-        <br />
-        <br />
-        <button id="button" onClick={this.loadFileAsText}>
-          Load Selected File
-        </button>
+    
+        <div class="split right">
+          <div class="centered">
+          <img src="img_avatar.png" alt="MiniCalendar"/>
+          <h2>Mini Calendar</h2>
+          <p>Only shows next week or 2</p>
+          </div>
+        </div> 
       </div>
     );
   }
@@ -60,7 +27,7 @@ class Menu extends React.Component {
 
 function MainMenu() {
   return (
-    <div id="notepad-page">
+    <div id="mainMenu">
       <Menu />
     </div>
   );
