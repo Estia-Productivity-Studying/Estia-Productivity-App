d5a0ba5b68978ce668cb4fd098462623428cdcdd
Hy-5 <coulbaly.ismael@gmail.com>
2021-03-29 03:31:34 -0500

    added a logout button to the main menu

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -1,17 +1,40 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import "./css/mainMenu.css";
 import events from './events'
+import Link from "@material-ui/core/Link";
+import Splash from "../Splash"
+
+
+function handleClick(event) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Splash />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 class Menu extends React.Component {
 
   render() {
     return(
-      <div class="split right">
-          <div class="centered">
-          <h2>Mini Calendar</h2>
-          <p>Only shows next week or 2</p>
-          </div>
-        </div> 
+      <div>
+        <Link
+          id="login-link"
+          variant="body2"
+          color="secondary"
+          onClick={handleClick}
+        >
+          LOGOUT
+        </Link>
+        <div class="split right">
+            <div class="centered">
+            <h2>Mini Calendar</h2>
+            <p>Only shows next week or 2</p>
+            </div>
+          </div> 
+      </div>
     );
   }
 }
